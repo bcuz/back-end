5827378e40e9acc64f251d00d59ce8803199df79
Andrew <andrewryanogle@gmail.com>
2019-04-27 10:29:52 -0700

    deployment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const server = require("./api/server");
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 server.get("/", (req, res) => {
   res.send("hi");
