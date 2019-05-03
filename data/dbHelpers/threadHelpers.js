48683caff08ac4f5117dedc51c09668a31b3e4e6
Andrew <andrewryanogle@gmail.com>
2019-05-02 21:29:26 -0700

    fixed thread with student id route error

--- data/dbHelpers/threadHelpers.js
+++ data/dbHelpers/threadHelpers.js
@@ -19,7 +19,7 @@ module.exports = {
   getThreadWithCommentById: async thread => {
     return db("comments")
       .select("comments.*", "students.username","bubl.bublName","threads.title")
-      .join("students", "students.id", "threads.student_id")
+      .join("students", "students.id", "comments.student_id",)
       .join("bubl", "threads.bubl_id", "bubl.id")
       .join("threads", "threads.id", "comments.thread_id")
       .where({ "comments.thread_id": thread })
